71bea413baf3bb9161157ee1c35b823bda18a571
VladKurluk <vlad11kurluk89@gmail.com>
2019-03-16 21:43:39 +0200

    Add 404 template and custom stylesheet file

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -38,7 +38,7 @@ def create_post():
 @posts.route('/<slug>/edit/', methods=['POST', 'GET'])
 @login_required
 def edit_post(slug):
-    post = Post.query.filter(Post.slug==slug).first()
+    post = Post.query.filter(Post.slug==slug).first_or_404()
 
     if request.method == 'POST':
         form = PostForm(formdata = request.form, obj = post)
@@ -75,13 +75,13 @@ def index():
 # http://localhost/blog/first-post
 @posts.route('/<slug>')
 def post_detail(slug):
-    post = Post.query.filter(Post.slug == slug).first()
+    post = Post.query.filter(Post.slug == slug).first_or_404()
     tags = post.tags
     return render_template('posts/post_detail.html', post=post, tags=tags)
 
 # http://localhost/blog/first-post/tag/python
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
-    tag = Tag.query.filter(Tag.slug == slug).first()
+    tag = Tag.query.filter(Tag.slug == slug).first_or_404()
     posts = tag.posts.all()
     return render_template('posts/tag_detail.html', tag=tag, posts=posts)
